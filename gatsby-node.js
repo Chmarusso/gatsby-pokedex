de604a92bb4c4c8cdcef9bcd436f059d5fb81248
Artur Ch <chmarus@gmail.com>
2019-01-25 20:56:54 +0100

    Pokemon pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,30 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require(`path`)
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return graphql(`
+    query {
+      pokedex {
+        pokemons(first: 1000) {
+          name, id, image, types, evolutions {
+            name
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.pokedex.pokemons.forEach(pokemon => {
+      createPage({
+        path: `/pokemon/${pokemon.name}`,
+        component: path.resolve(`./src/templates/pokemon.js`),
+        context: {
+          name: pokemon.name,
+        },
+      })
+    })
+  })
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,15 +9,14 @@ const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <article>
-      {console.log(data)}
       <div className="cf pa2">
         {data.pokedex.pokemons.map(pokemon =>
-          <div className="fl w-50 w-25-m w-20-l pa2 h5">
-            <a href="/asd" className="db link dim tc">
+          <div key={pokemon.id} className="fl w-50 w-25-m w-20-l pa2 h5">
+            <a href={`/pokemon/${pokemon.name}`} className="db link dim tc">
               <img style={{maxHeight: '150px'}} src={pokemon.image} alt={pokemon.name} className="h-100 db outline black-10"/>
               <dl className="mt2 f6 lh-copy">
                 <dt className="clip">Name</dt>
-                <dd class="ml0 black truncate w-100">{pokemon.name}</dd>
+                <dd className="ml0 black truncate w-100">{pokemon.name}</dd>
               </dl>
             </a>
           </div>
